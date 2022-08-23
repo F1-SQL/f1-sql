4a9337538b9ee3bf0421a70f8ea6b215bebd1a54
Rich In SQL <github@codenameowl.com>
2022-08-23 09:28:41 +0100

    Removed: Old code removed
    Removed: Old code removed

--- productionDatabase/1.1 Ref CircuitDirections.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO [Ref].[CircuitDirections] ([Direction],[DirectionRefID])
-VALUES
-('Clockwise',1),
-('Anti-clockwise',2),
-('Part clockwise and part anti-clockwise (figure eight)',3)
--- productionDatabase/1.2 Ref CircuitType.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO [Ref].[CircuitType]([CircuitType],[TypeRefID])
-VALUES
-('Street circuit',1),
-('Road circuit',2),
-('Street circuit',3),
-('Race circuit',4)
