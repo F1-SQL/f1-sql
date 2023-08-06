d4ff9a496b1d76cd4ea5f18879e06e54fdc3475d
Rich Howell <github@codenameowl.com>
2023-08-06 09:24:52 +0100

    Update build_database.ps1

--- build_database.ps1
+++ build_database.ps1
@@ -1,20 +1,84 @@
+#Requires -Modules @{ ModuleName="dbatools"; ModuleVersion="2.0.4" }
+
+<#
+    .SYNOPSIS
+        Build a SQL Server database using Ergast Formula One CSV data.
+
+    .DESCRIPTION
+        Performs a backup of a specified type of 1 or more databases on a single SQL Server Instance. These backups may be Full, Differential or Transaction log backups.
+        
+    .PARAMETER sqlInstance
+        The SQL Server instance hosting the databases to be backed up.
+
+    .PARAMETER databaseName
+        This is the name of the database you wish to create.
+
+    .PARAMETER cleanInstance
+        Removes the database from the instance once complete, this will only be processed if backupDatabase is true.
+
+    .PARAMETER backupDatabase
+        Performs a backup of the database
+
+    .NOTES
+        Tags: FormulaOne, F1, Database, Data.
+        Author: Richard Howell, richinsql.com
+
+        Website: https://richinsql.com
+        Copyright: (c) 2022 by Rich In SQL, licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .LINK
+        https://richinsql.com/projects/formula-one-database/
+
+    .EXAMPLE
+        PS C:\> .\build_database.ps1 -databaseName f1db -sqlInstance 'loclhost' -downloadFiles $true -cleanInstance $false
+
+        This will perform a full database backup on the databases HR and Finance on SQL Server Instance Server1 to Server1 default backup directory.
+
+    .EXAMPLE
+        PS C:\> .\build_database.ps1 -databaseName f1db -sqlInstance 'loclhost' -downloadFiles $true -cleanInstance $true
+
+        Backs up AdventureWorks2014 to sql2016 C:\temp folder.
+
+    .EXAMPLE
+        PS C:\> .\build_database.ps1 -databaseName f1db -sqlInstance 'loclhost' -downloadFiles $false -cleanInstance $true
+
+        Performs a full backup of all databases on the sql2016 instance to their own containers under the https://dbatoolsaz.blob.core.windows.net/azbackups/ container on Azure blob storage using the sql credential "dbatoolscred" registered on the sql2016 instance.
+
+    .EXAMPLE
+        PS C:\> .\build_database.ps1 -databaseName f1db -sqlInstance 'loclhost' -downloadFiles $false -cleanInstance $false
+
+        Performs a full backup of all databases on the sql2016 instance to the https://dbatoolsaz.blob.core.windows.net/azbackups/ container on Azure blob storage using the Shared Access Signature sql credential "https://dbatoolsaz.blob.core.windows.net/azbackups" registered on the sql2016 instance.
+    #>
+
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$false)]
     [System.String]
-    $databaseName,
+    $sqlInstance,
     [Parameter(Mandatory=$True, Position=1, ValueFromPipeline=$false)]
     [System.String]
-    $sqlInstance,
+    $databaseName,
     [Parameter(Mandatory=$True, Position=2, ValueFromPipeline=$false)]
     [System.Boolean]
-    $downloadFiles   
+    $cleanInstance,
+    [Parameter(Mandatory=$True, Position=3, ValueFromPipeline=$false)]
+    [System.Boolean]
+    $backupDatabase   
 )
 
-$raceName = Read-Host -Prompt "Please tell me the name of this race E.G. Silverstone 2023"
-$raceName = $raceName.Replace(' ','_')
+Write-Host "INFO: Atempting to open a connection to $sqlInstance ..." -ForegroundColor Yellow
+$svr = Connect-dbaInstance -SqlInstance $sqlInstance
 
-$runDate = (Get-Date).Date.ToString("yyyy-MM-dd")
+$currentYear = (Get-Date).Year.ToString()
+$version = Get-DbaBuildReference -SqlInstance $svr | Select-Object -ExpandProperty NameLevel
+
+$races=@("Bahrain","Saudi Arabia","Australia","Azerbaijan","United States","Monaco","Spain","Canada","Austria","Great Britain","Hungary","Belgium","Italy","Belgium","Japan","Qatar","Austin","Mexico","Brazil","Las Vegas","Abu Dhabi")
+$raceName = $races | Out-GridView -PassThru
+
+$raceName = $raceName.Replace(' ','_')
+$raceName += "_" + $currentYear
+
 $rootpath = $PSScriptRoot
 
 $sourceFiles = "\sourcefiles\"
@@ -22,15 +86,17 @@ $sourceFilesFullPath = $rootpath + $sourceFiles
 
 $archiveFolder = "\archivedfiles\"
 $archiveLocation = $rootpath + $archiveFolder
-$archiveLocationDate = $archiveLocation + $runDate + "\"
+$archiveLocationDate = $archiveLocation + $raceName + "\"
 
-$backupName = $databaseName + "_" + $raceName + "_" + $runDate + ".bak"
+$backupName = $version + "_" + $databaseName + "_" + $raceName + ".bak"
 $backupFolder = "\backups\"
-$backupLocation = $rootpath + $backupFolder
+$backupRaceFolder = $backupFolder + $raceName
+$backupLocation = $rootpath + $backupRaceFolder
+$backupFullPath = $backupLocation + "\" + $backupName
 
 $sourceLocation = "https://ergast.com/downloads/f1db_csv.zip"
 
-$zipName = 'f1db_csv_' + $runDate + '.zip'
+$zipName = 'f1db_csv_' + $raceName + '.zip'
 $zipLocation = $rootpath + $sourceFiles 
 $zipLocationFull = $zipLocation + $zipName
 
@@ -70,13 +136,13 @@ if(-Not(Test-Path -Path $sourceFilesFullPath))
     Write-Host "INFO: The directory $sourceFilesFullPath already exists, skipping creation" -ForegroundColor Yellow
 }
 
-$existingFiles = Get-ChildItem -Path $sourceFilesFullPath -Recurse
+$existingFiles = Get-ChildItem -Path $sourceFilesFullPath -Filter *.csv -Recurse
 
 foreach($fileName in $existingFiles)
 {
     $file = [io.path]::GetFileNameWithoutExtension($fileName)
     $extension = [io.path]::GetExtension($fileName)
-    $newName = $archiveLocationDate + $file + "_" + $raceName + "_" + $runDate + $extension
+    $newName = $archiveLocationDate + $file + "_" + $raceName +  $extension
     Write-Host "INFO: Moving $filename to the archive" -ForegroundColor Yellow
     Move-Item -Path $filename -Destination $newName -Force
     
@@ -89,30 +155,37 @@ foreach($fileName in $existingFiles)
 
 if($downloadFiles -eq $true)
 {
-    Write-Host "INFO: Attempting to download zip file from $sourceLocaiton to $ziplocation" -ForegroundColor Yellow
-    Invoke-WebRequest -Uri $sourceLocation -OutFile $zipLocationFull
+    if(-Not(Test-Path $zipLocationFull))
+    {
+        Write-Host "INFO: Zip file $zipName does not exist" -ForegroundColor Yellow
+        Write-Host "INFO: Attempting to download zip file from $sourceLocaiton to $ziplocation" -ForegroundColor Yellow
+        Invoke-WebRequest -Uri $sourceLocation -OutFile $zipLocationFull
+    } else {
+        Write-Host "INFO: Zip file $zipName already exists will not re-download" -ForegroundColor Yellow
+    }
 }
 
 if(Test-Path $zipLocationFull -PathType Leaf)
 {
     Write-Host "INFO: Attempting to extract files from $zipLocationFull into $sourceFilesFullPath" -ForegroundColor Yellow
-    Expand-Archive $zipLocationFull -DestinationPath $sourceFilesFullPath
-    Write-Host "INFO: Deleting $zipLocationFull" -ForegroundColor Yellow
-    Remove-Item $zipLocationFull -Force
+    Expand-Archive $zipLocationFull -DestinationPath $sourceFilesFullPath    
 } else {
     Write-Host "WARN: Zip file does not exist in $zipLocation" -ForegroundColor Red
     Exit 
 }
 
-$svr = Connect-dbaInstance -SqlInstance $sqlInstance
 $database = Get-DbaDatabase -SqlInstance $svr -Database $databaseName
 
-if($null -ne $database)
+if($database)
 {
-    Write-Host "INFO: Atempting to open a connection to $sqlInstance ..." -ForegroundColor Yellow
     Write-Host "WARN: Database already exists $databaseName from" $sqlInstance -ForegroundColor Red
     Write-Host "INFO: Attempting to drop $databaseName from" $sqlInstance -ForegroundColor Yellow
     Remove-DbaDatabase -SqlInstance $svr -Database $databaseName -Confirm:$false
+    Write-Host "INFO: Attempting to create $databaseName" -ForegroundColor Yellow
+    New-DbaDatabase -SqlInstance $svr -Name $databaseName
+    Write-Host "SUCESS: Database" $databaseName" created" -ForegroundColor Green
+} else 
+{
     Write-Host "INFO: Attempting to create $databaseName" -ForegroundColor Yellow
     New-DbaDatabase -SqlInstance $svr -Name $databaseName
     Write-Host "SUCESS: Database" $databaseName" created" -ForegroundColor Green
@@ -161,7 +234,9 @@ foreach($renamedfile in $allFiles)
     }  
 }
 
-if($null -ne $database)
+$database = Get-DbaDatabase -SqlInstance $svr -Database $databaseName
+
+if($database)
 {
     Write-Host "INFO: Creating tables" -ForegroundColor Yellow
     Invoke-DbaQuery -SqlInstance $svr -File ('{0}\f1db_tables.sql' -f $rootpath)
@@ -181,6 +256,7 @@ if($null -ne $database)
         Import-DbaCsv -Path $filePath -SqlInstance $svr -Database $databaseName -Table $fileWithoutExtension -Delimiter "," -NoProgress -KeepIdentity
     }
 } else {
+    
     Write-Host "WARN: Creating tables not possible, $databaseName doesn't exist" -ForegroundColor Red
     Exit
 }
@@ -192,6 +268,22 @@ if($null -eq $database)
     Invoke-DbaQuery -SqlInstance $svr -File ('{0}\f1db_foreign_keys.sql' -f $rootpath)
 } 
 
-Write-Host "SUCCESS: Database build has been completed" -ForegroundColor Green
+if(Test-Path -Path $backupFullPath)
+{
+    Write-Host "WARN: Database backup already exists, removing" -ForegroundColor Red
+    Remove-Item -Path $backupFullPath
+}
 
-Backup-DbaDatabase -SqlInstance $svr -Database $databaseName -Path $backupLocation -FilePath $backupName -CompressBackup -Type Full
+Backup-DbaDatabase -SqlInstance $svr -Database $databaseName -Path $backupLocation -FilePath $backupName -Type Full 
+Write-Host "INFO: Database backup has been completed" -ForegroundColor Yellow
+Write-Host "SUCCESS: Database backup has been completed" -ForegroundColor Green
+
+Write-Host "SUCCESS: Database build complete" -ForegroundColor Green
+
+if($cleanInstance -eq $True -and $backupDatabase -eq $True)
+{
+    Write-Host "INFO: Dropping database $databaseName from $sqlInstance" -ForegroundColor Yellow
+    Remove-DbaDatabase -SqlInstance $svr -Database $databaseName -Confirm:$false 
+} else {
+    Write-Host "WARN: $databaseName not dropped as database is not set to backup"
+}
