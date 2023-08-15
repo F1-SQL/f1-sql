7d78fce5491ae25e98fc51f038b155728b661331
Rich Howell <github@codenameowl.com>
2023-08-15 09:48:09 +0100

    Update SequelFormula_data_updates.sql
    Number of bug fixes resolved.
    
    New columns moved to the Tables .sql file

--- src/SequelFormula_data_updates.sql
+++ src/SequelFormula_data_updates.sql
@@ -1,7 +1,3 @@
-ALTER TABLE [dbo].[drivers] ADD nationalityID INT 
-
-GO
-
 UPDATE d 
 
 SET d.nationalityID = n.nationalityID 
@@ -9,15 +5,6 @@ SET d.nationalityID = n.nationalityID
 FROM [dbo].[drivers] d
 
 INNER JOIN [dbo].[nationalities] n ON d.nationality = n.nationality
-
-
-GO
-
-ALTER TABLE [dbo].[drivers] DROP COLUMN nationality 
-
-GO
-
-ALTER TABLE [dbo].[constructors] ADD nationalityID INT 
 
 GO
 
@@ -31,14 +18,6 @@ INNER JOIN dbo.nationalities n ON c.nationality = n.nationality
 
 GO
 
-ALTER TABLE [dbo].[constructors] DROP COLUMN nationality 
-
-GO
-
-ALTER TABLE dbo.Results ADD positionTextID INT;
-
-GO
-
 UPDATE r 
 
  SET r.positionTextID = pt.positionTextID 
@@ -49,33 +28,13 @@ INNER JOIN [dbo].[positionText] pt ON r.[positionText] = pt.[positioncode]
 
 GO
 
-ALTER TABLE [dbo].[results] DROP COLUMN postitionText 
-
-GO
-
-ALTER TABLE [dbo].[sprintResults] ADD positionTextID INT;
-
-GO
-
 UPDATE sr 
 
  SET sr.positionTextID = pt.positionTextID 
 
 FROM [dbo].[sprintResults] sr
 
-INNER JOIN dbo.positionText pt ON sr.positionText = pt.positioncode
-
-GO
-
-ALTER TABLE [dbo].[sprintResults] DROP COLUMN postitionText 
-GO
-ALTER TABLE [dbo].[positionText] DROP COLUMN postitionCode
-
-GO
-
-ALTER TABLE [dbo].[circuits] ADD countryID INT;
-ALTER TABLE [dbo].[circuits] ADD circuitDirectionID INT;
-ALTER TABLE [dbo].[circuits] ADD circuitTypeID INT;
+INNER JOIN dbo.positionText pt ON sr.positionText = pt.positionText
 
 GO
 
@@ -88,15 +47,6 @@ FROM [dbo].[circuits] cir
 INNER JOIN [dbo].[countries] c ON cir.[country] = c.[country]
 
 GO
-ALTER TABLE [dbo].[circuits] DROP CONSTRAINT DF_circuits_country; 
-GO
-ALTER TABLE [dbo].[circuits] DROP COLUMN country; 
-
-GO
-
-ALTER TABLE [dbo].[circuits] ADD locationID INT;
-
-GO
 
 UPDATE cir
 
@@ -107,12 +57,8 @@ FROM [dbo].[circuits] cir
 INNER JOIN [dbo].[locations] l ON cir.[location] = l.[locationName]
  
 GO
-ALTER TABLE [dbo].[circuits] DROP COLUMN location; 
-
-GO
 
 UPDATE c SET 
-
 
 c.circuitDirectionID = tc.circuitDirectionID,
 c.circuitTypeID = tc.circuitTypeID
@@ -125,3 +71,27 @@ INNER JOIN [dbo].[circuits] c ON c.name = tc.circuit
 GO
 
 DROP TABLE [dbo].[tempCircuits]
+
+GO
+
+UPDATE cr
+
+SET 
+	cr.positionTextID = pt.positionTextID
+
+FROM 
+	[dbo].[constructorResults] cr
+
+INNER JOIN [dbo].[positionText] pt 
+	ON cr.status = pt.positionCode
+
+GO
+
+ALTER TABLE [dbo].[constructors] DROP COLUMN [nationality]; 
+ALTER TABLE [dbo].[circuits] DROP COLUMN [location]; 
+ALTER TABLE [dbo].[circuits] DROP COLUMN [country]; 
+ALTER TABLE [dbo].[results] DROP COLUMN [positionText];
+ALTER TABLE [dbo].[drivers] DROP COLUMN [nationality]; 
+ALTER TABLE [dbo].[sprintResults] DROP COLUMN [positionText];
+ALTER TABLE [dbo].[positionText] DROP COLUMN [positionText];
+ALTER TABLE [dbo].[constructorResults] DROP COLUMN [status];
