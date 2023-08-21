d09cec0c3f82ad18b9053a4f131a068ac07b8af3
Rich Howell <github@codenameowl.com>
2023-08-21 19:40:06 +0100

    Data Update Clean

--- src/SequelFormula_data_updates.sql
+++ src/SequelFormula_data_updates.sql
@@ -100,37 +100,142 @@ INNER JOIN [dbo].[positionText] pt
 
 GO
 
+;WITH LeadingResults AS
+(
+	SELECT 
+
+	raceID,
+	positionOrder,
+	dateadd(ms, milliseconds, '19800101') as LeadTime
+
+	FROM 
+		[dbo].[results]	
+	WHERE 
+		positionOrder = 1
+),
+DataOutput AS 
+(
+	SELECT 
+		resultID,
+		r.raceID,
+		r.positionOrder,
+		milliseconds,
+		time,
+		dateadd(ms, r.milliseconds, '19800101') - CASE 
+													WHEN r.positionOrder = 1 THEN LAG(dateadd(ms, r.milliseconds, '19800101')) OVER( ORDER BY r.raceID,r.positionOrder) 
+													WHEN r.positionOrder != 1 THEN lr.LeadTime 
+												END AS Diff
+	FROM							
+		[dbo].[results]	r
+		
+		INNER JOIN LeadingResults lr 
+			ON r.raceId = lr.raceId
+)
+
+UPDATE r
+	SET 
+		r.TimeDifference = do.Diff
+	FROM 
+		[dbo].[results] r
+
+		INNER JOIN DataOutput DO 
+			ON r.resultId = do.resultId
+
+GO
 
 UPDATE [dbo].[results]
 	SET
 		fastestLapTime_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(fastestLapTime, ':', '')), 10), 5, 0, ':'), 3, 0, ':')) 
+
+/*sprintResults*/
+
+;WITH LeadingResults AS
+(
+SELECT 
+
+raceID,
+positionOrder,
+dateadd(ms, milliseconds, '19800101') as LeadTime
+
 FROM 
-	[dbo].[results]
+	[dbo].[sprintResults]	
+WHERE 
+	positionOrder = 1
+),
+DataOutput AS 
+(
+SELECT 
+	resultID,
+	r.raceID,
+	r.positionOrder,
+	milliseconds,
+	time,
+	dateadd(ms, r.milliseconds, '19800101') - CASE 
+												WHEN r.positionOrder = 1 THEN LAG(dateadd(ms, r.milliseconds, '19800101')) OVER( ORDER BY r.raceID,r.positionOrder) 
+												WHEN r.positionOrder != 1 THEN lr.LeadTime 
+											END AS Diff
+FROM							
+	[dbo].[sprintResults]	r
+	
+	INNER JOIN LeadingResults lr 
+		ON r.raceId = lr.raceId
+)
+
+UPDATE r
+	SET 
+		r.TimeDifference = do.Diff
+
+	FROM 
+		[dbo].[sprintResults] r
+
+		INNER JOIN DataOutput DO ON r.resultId = do.resultId
 
 
+UPDATE [dbo].[sprintResults]
+	SET
+		fastestLapTime_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(fastestLapTime, ':', '')), 10), 5, 0, ':'), 3, 0, ':')) 
+
+GO
 
 UPDATE [dbo].[results]
-SET 
-	fastestLapSpeed_Decimal = TRY_CONVERT(decimal(18,3),fastestLapSpeed) 
-FROM 
-	[dbo].[results]
+	SET
+		fastestLapTime_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(fastestLapTime, ':', '')), 10), 5, 0, ':'), 3, 0, ':')) 
 
+GO
+
+UPDATE [dbo].[results]
+	SET 
+		fastestLapSpeed_Decimal = TRY_CONVERT(decimal(18,3),fastestLapSpeed) 
+
+GO
 
 UPDATE [dbo].[pitStops] 
 	SET
 		duration_converted = TRY_CONVERT(decimal(18,3),duration)
 
 UPDATE [dbo].[qualifying]
-SET
-	q1_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q1, ':', '')), 10), 5, 0, ':'), 3, 0, ':')),
-	q2_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q2, ':', '')), 10), 5, 0, ':'), 3, 0, ':')),
-	q3_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q3, ':', '')), 10), 5, 0, ':'), 3, 0, ':'))
+	SET
+		q1_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q1, ':', '')), 10), 5, 0, ':'), 3, 0, ':')),
+		q2_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q2, ':', '')), 10), 5, 0, ':'), 3, 0, ':')),
+		q3_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q3, ':', '')), 10), 5, 0, ':'), 3, 0, ':'));
 
 GO
 
 UPDATE [dbo].[lapTimes]
 	SET
-		time_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(time, ':', '')), 10), 5, 0, ':'), 3, 0, ':')) 
+		time_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(time, ':', '')), 10), 5, 0, ':'), 3, 0, ':')); 
+
+GO
+
+UPDATE [dbo].[results] 
+	SET 
+		time_converted = TRY_CONVERT(time(3),[time]) WHERE position = 1;
+
+GO
+
+UPDATE [dbo].[results] 
+	SET 
+		time_converted = TRY_CONVERT(time(3),[TimeDifference]) WHERE position != 1;
 
 GO
 
