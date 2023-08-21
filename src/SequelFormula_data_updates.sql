c9958b7180da24fbefead77acd5c530c8adc8b95
Rich Howell <github@codenameowl.com>
2023-08-21 18:47:41 +0100

    Formatting changes
    Changes to formatting

--- src/SequelFormula_data_updates.sql
+++ src/SequelFormula_data_updates.sql
@@ -126,8 +126,6 @@ SET
 	q2_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q2, ':', '')), 10), 5, 0, ':'), 3, 0, ':')),
 	q3_converted = TRY_CONVERT(time, STUFF(STUFF(RIGHT(CONCAT('000000', REPLACE(q3, ':', '')), 10), 5, 0, ':'), 3, 0, ':'))
 
-
-
 GO
 
 ALTER TABLE [dbo].[constructors] DROP COLUMN [nationality]; 
@@ -141,29 +139,18 @@ ALTER TABLE [dbo].[constructorResults] DROP COLUMN [status];
 ALTER TABLE [dbo].[constructorStandings] DROP COLUMN [positionText];
 ALTER TABLE [dbo].[constructorStandings] DROP COLUMN [positionText];
 ALTER TABLE [dbo].[driverStandings] DROP COLUMN [positionText];
-
 ALTER TABLE [dbo].[sprintResults] DROP COLUMN [fastestLapTime];
-
-EXEC sp_rename 'dbo.sprintResults.fastestLapTime_converted', 'fastestLapTime', 'COLUMN';
-
 ALTER TABLE [dbo].[results] DROP COLUMN [fastestLapTime];
-
-EXEC sp_rename 'dbo.results.fastestLapTime_converted', 'fastestLapTime', 'COLUMN';
-
-
 ALTER TABLE [dbo].[results] DROP COLUMN [fastestLapSpeed];
-
-EXEC sp_rename 'dbo.results.fastestLapSpeed_Decimal', 'fastestLapSpeed', 'COLUMN';
-
-
 ALTER TABLE [dbo].[pitStops] DROP COLUMN [duration];
-
-EXEC sp_rename 'dbo.pitStops.duration_converted', 'duration', 'COLUMN';
-
 ALTER TABLE [dbo].[results] DROP COLUMN q1;
 ALTER TABLE [dbo].[results] DROP COLUMN q2;
 ALTER TABLE [dbo].[results] DROP COLUMN q3;
 
+EXEC sp_rename 'dbo.sprintResults.fastestLapTime_converted', 'fastestLapTime', 'COLUMN';
+EXEC sp_rename 'dbo.results.fastestLapTime_converted', 'fastestLapTime', 'COLUMN';
+EXEC sp_rename 'dbo.results.fastestLapSpeed_Decimal', 'fastestLapSpeed', 'COLUMN';
+EXEC sp_rename 'dbo.pitStops.duration_converted', 'duration', 'COLUMN';
 EXEC sp_rename 'dbo.qualifying.q1_converted', 'q1', 'COLUMN';
 EXEC sp_rename 'dbo.qualifying.q2_converted', 'q2', 'COLUMN';
 EXEC sp_rename 'dbo.qualifying.q3_converted', 'q3', 'COLUMN';
